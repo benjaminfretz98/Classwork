20f6d63cb4e7c8698a8819b8ad5704ac21bc2cc7
bfretz <benjaminfretz98@gmail.com>
2020-02-20 07:47:36 +0000

    added new script because I deleted the old one by accident

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,24 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+echo "Howdy there brotherman, we're gonna create you a secure passwordless SSH user"
+read -p "What would you like the name of your new account to be? " name
+sleep 1
+echo "Alright sounds good, an account named $name has been created"
+sleep 1
+
+echo "Creating home directory .ssh folder"
+sudo mkdir /home/"$name"/.ssh
+echo "Done"
+
+echo "Copying public RSA key from repository"
+sudo cp /home/bfretz/Classwork/SYS265/linux/public-keys/id_rsa.pub /home/"$name"/.ssh/authorized_keys
+echo "Done"
+
+echo "Setting correct permissions"
+sudo chmod 700 /home/"$name"/.ssh
+sudo chmod 600 /home/"$name"/.ssh/authorized_keys
+sudo chown -R "$name":"$name" /home/"$name"/.ssh
+echo "Done"
+
+
