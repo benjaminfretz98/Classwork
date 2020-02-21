f1752aebd9488404cf219aa681343eac6978b058
bfretz <benjaminfretz98@gmail.com>
2020-02-21 06:02:04 +0000

    gr

--- SYS265/linux/centos7/letsgo.sh
+++ SYS265/linux/centos7/letsgo.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 sudo useradd -m -d /home/$1 -s /bin/bash $1
 sudo mkdir /home/$1/.ssh
-sudo cp /home/bfretz/Classwork/SYS265/linux/public-keys/is_rsa_4.pub /home/$1/.ssh/authorized_keys
+sudo cp /home/bfretz/Classwork/SYS265/linux/public-keys/id_rsa_4.pub /home/$1/.ssh/authorized_keys
 sudo chmod 700 /home/$1/.ssh
 sudo chmod 600 /home/$1/.ssh/authorized_keys
 sudo chown -R $1:$1 /home/$1/.ssh
